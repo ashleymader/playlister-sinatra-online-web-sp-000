2315b2fcb06dbdf8fdc7cf446f253a87040b76f4
ashleymader <ashley.mader12@gmail.com>
2020-05-09 15:18:37 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -22,6 +22,7 @@ use Rack::Flash
         @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
         @song.genre_ids = params[:genres]
         @song.save
+        binding.pry
         flash[:message] = "Successfully created song."
         redirect "/songs/#{@song.slug}" 
       end
